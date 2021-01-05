042d1b5f13ef5b8464a96ed71bcd6f78b1fbd23e
zhangruisong <zhangruisong@yuzhilin.net.cn>
2021-01-05 21:49:13 +0800

    增加顾客级别

--- yzl-crm-customer-api/src/main/java/cn/net/yzl/crm/customer/dao/MemberMapper.java
+++ yzl-crm-customer-api/src/main/java/cn/net/yzl/crm/customer/dao/MemberMapper.java
@@ -1,5 +1,7 @@
 package cn.net.yzl.crm.customer.dao;
 
+import cn.net.yzl.crm.customer.config.db.DataSourceSelector;
+import cn.net.yzl.crm.customer.config.db.DynamicDataSourceEnum;
 import cn.net.yzl.crm.customer.dto.member.MemberSerchConditionDTO;
 import cn.net.yzl.crm.customer.model.Member;
 import cn.net.yzl.crm.customer.model.MemberGrad;
@@ -11,6 +13,7 @@ import java.util.List;
 public interface MemberMapper {
     List<MemberGrad> getMemberGrad();
 
+    @DataSourceSelector(value = DynamicDataSourceEnum.master)
     int insert(Member record);
 
     List<Member> findPageByCondition(MemberSerchConditionDTO dto);
