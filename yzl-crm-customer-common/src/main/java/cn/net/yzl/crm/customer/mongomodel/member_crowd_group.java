05297dc4b662f5188034527a7ea4e9ed504a3327
zhangruisong <zhangruisong@yuzhilin.net.cn>
2021-01-16 10:27:30 +0800

    引入MongoRepository

--- yzl-crm-customer-common/src/main/java/cn/net/yzl/crm/customer/mongomodel/member_crowd_group.java
+++ yzl-crm-customer-common/src/main/java/cn/net/yzl/crm/customer/mongomodel/member_crowd_group.java
@@ -152,8 +152,8 @@ public class member_crowd_group {
 
     @ApiModelProperty("是否活动订单，1是，0否，-1不做统计查询")
     private Integer active_order;
-//    @ApiModelProperty("活动")
-//    private List<String> active_ids;
+    @ApiModelProperty("活动")
+    private List<crowd_active> crowd_active;
 //    @ApiModelProperty("活动类型")
 //    private List<String> active_type;
 
@@ -196,6 +196,29 @@ public class member_crowd_group {
     @ApiModelProperty("圈选病症")
     private List<crowd_disease> diseases;
 
+    public List<cn.net.yzl.crm.customer.mongomodel.crowd_active> getCrowd_active() {
+        return crowd_active;
+    }
+
+    public void setCrowd_active(List<cn.net.yzl.crm.customer.mongomodel.crowd_active> crowd_active) {
+        this.crowd_active = crowd_active;
+    }
+
+    public List<crowd_base_value> getLogistics_state() {
+        return logistics_state;
+    }
+
+    public void setLogistics_state(List<crowd_base_value> logistics_state) {
+        this.logistics_state = logistics_state;
+    }
+
+    public List<crowd_base_value> getLogistics_company_id() {
+        return logistics_company_id;
+    }
+
+    public void setLogistics_company_id(List<crowd_base_value> logistics_company_id) {
+        this.logistics_company_id = logistics_company_id;
+    }
 
     public Date getEffective_date() {
         return effective_date;
