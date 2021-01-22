bea4f8f7767173790bb60f384ea54c8827a0f186
lichanghong <lichanghong@yuzhilin.net.cn>
2021-01-22 15:38:12 +0800

    修改实体

--- yzl-crm-customer-common/src/main/java/cn/net/yzl/crm/customer/mongomodel/member_crowd_group.java
+++ yzl-crm-customer-common/src/main/java/cn/net/yzl/crm/customer/mongomodel/member_crowd_group.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class member_crowd_group extends BaseObject {
 
     @ApiModelProperty("群组id")
-    private String crowd_id;
+    private String _id;
 
     @ApiModelProperty("群组名称")
     private String crowd_name;
