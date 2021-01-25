47fd201e3fc3ecf268f3ac76e0d43ad1cc548a44
tangwenjie <tangwenjie@yuzhilin.net.cn>
2021-01-25 15:08:45 +0800

    综合行为mapper文件上传

--- yzl-crm-customer-api/src/main/java/cn/net/yzl/crm/customer/dao/MemberActionRelationMapper.java
+++ yzl-crm-customer-api/src/main/java/cn/net/yzl/crm/customer/dao/MemberActionRelationMapper.java
@@ -3,7 +3,7 @@ package cn.net.yzl.crm.customer.dao;
 
 import cn.net.yzl.crm.customer.viewmodel.memberActionModel.MemberActionRelation;
 
-public interface MemberActionRelationDao {
+public interface MemberActionRelationMapper {
     int deleteByPrimaryKey(Integer id);
 
     int insert(MemberActionRelation record);
