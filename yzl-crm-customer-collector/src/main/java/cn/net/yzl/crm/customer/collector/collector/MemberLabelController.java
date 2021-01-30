68b16830569708b10ae204b997aa03bf1015873c
lichanghong <lichanghong@yuzhilin.net.cn>
2021-01-30 09:15:39 +0800

    修改接口描述

--- yzl-crm-customer-collector/src/main/java/cn/net/yzl/crm/customer/collector/collector/MemberLabelController.java
+++ yzl-crm-customer-collector/src/main/java/cn/net/yzl/crm/customer/collector/collector/MemberLabelController.java
@@ -1,6 +1,8 @@
 package cn.net.yzl.crm.customer.collector.collector;
 
 import cn.net.yzl.crm.customer.collector.service.MemberLabelService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,12 +20,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberLabelController {
     @Autowired
     private MemberLabelService memberLabelService;
-    @ApiOperation("同步")
+    @ApiOperation("根据顾客表的下单时间同步")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "timestamp", value = "时间戳，以毫秒为单位", required = true, dataType = "long", paramType = "query")
+    })
     @GetMapping("/member/syncMember")
     public boolean syncMember(long timestamp){
     return memberLabelService.syncMember(timestamp);
     }
-    @ApiOperation("同步")
+
+
+    @ApiOperation("根据进线时间同步")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "通话记录表中的主键", required = true, dataType = "int", paramType = "query")
+    })
     @GetMapping("/member/syncById")
     public boolean syncById(@RequestParam(defaultValue = "0") int id){
         return memberLabelService.syncById(id);
