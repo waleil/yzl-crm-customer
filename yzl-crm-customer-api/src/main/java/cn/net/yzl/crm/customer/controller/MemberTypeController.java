309408b92a7a44657985c28dc5ca2058b21f4999
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-02-03 21:15:26 +0800

    修改注解

--- yzl-crm-customer-api/src/main/java/cn/net/yzl/crm/customer/controller/MemberTypeController.java
+++ yzl-crm-customer-api/src/main/java/cn/net/yzl/crm/customer/controller/MemberTypeController.java
@@ -3,11 +3,12 @@ package cn.net.yzl.crm.customer.controller;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.crm.customer.dto.member.MemberTypeDTO;
 import cn.net.yzl.crm.customer.service.MemberTypeService;
-import cn.net.yzl.crm.customer.vo.MemberTypeVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,14 +20,19 @@ import java.util.List;
  * @description 顾客类别查询
  * @date: 2021/2/3 5:15 下午
  */
+@RestController
+@RequestMapping("/memberType")
 @Api(value = "顾客类型", tags = {"顾客类型"})
-@RestController("/memberType")
+@Validated
 public class MemberTypeController {
+
     @Autowired
     private MemberTypeService memberTypeService;
+
+
     @ApiOperation(value = "顾客类别查询", notes = "顾客类别查询")
-    @GetMapping("/v1/queryMemberType")
-    public ComResponse<List<MemberTypeDTO>> queryMemberType(){
+    @RequestMapping(value = "/v1/queryMemberType", method = RequestMethod.GET)
+    public ComResponse<List<MemberTypeDTO>> queryMemberType() {
         List<MemberTypeDTO> list = memberTypeService.queryMemberType();
     return ComResponse.success(list);
     }
