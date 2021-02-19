abc666363d5941d2df36d2ed4d78840e377e0b25
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-02-19 15:19:15 +0800

    测试同步会员标签

--- yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/utils/date/DateUtil.java
+++ yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/utils/date/DateUtil.java
@@ -16,4 +16,15 @@ public class DateUtil {
         return calendar.getTime();
     }
 
+    //获取昨天（按当前传入的时区）00:00:00所对应时刻的long型值
+    public static Date getYesterdayStart() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE,-1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
 }
