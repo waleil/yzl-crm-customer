6aa6f67e6e25302257075ea9679d27006167a5d9
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-02-25 18:13:17 +0800

    修改根据广告商品圈选条件

--- yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/dao/mongo/MemberLabelDao.java
+++ yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/dao/mongo/MemberLabelDao.java
@@ -1168,7 +1168,7 @@ public class MemberLabelDao extends MongoBaseDao<MemberLabel> {
                 for (int i = 0; i < size; i++) {
                     crowd_product c = in.get(i);
                     Criteria criteria = new Criteria();
-                    and.add(Criteria.where("advertProducts").elemMatch(Criteria.where("productCode").is(c.getId())));
+                    and.add(Criteria.where("advertProducts").in(String.valueOf(c.getId())));
 
                 }
 //                criteria.andOperator(andArray);
@@ -1178,7 +1178,7 @@ public class MemberLabelDao extends MongoBaseDao<MemberLabel> {
                 for (int i = 0; i < size; i++) {
                     crowd_product c = ex.get(i);
                     Criteria criteria = new Criteria();
-                    not.add(Criteria.where("advertProducts").elemMatch(Criteria.where("productCode").is(c.getId())));
+                    not.add(Criteria.where("advertProducts").in(String.valueOf(c.getId())));
                 }
                 // criteria.norOperator(exArray);
             }
