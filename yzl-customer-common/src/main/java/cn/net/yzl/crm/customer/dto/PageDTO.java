c2dba7bebfb574fc895ba28ef3dffc81bbfec8e1
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-03-11 11:13:04 +0800

    优化顾客列表查询

--- yzl-customer-common/src/main/java/cn/net/yzl/crm/customer/dto/PageDTO.java
+++ yzl-customer-common/src/main/java/cn/net/yzl/crm/customer/dto/PageDTO.java
@@ -1,11 +1,13 @@
 package cn.net.yzl.crm.customer.dto;
 
 
+import cn.net.yzl.common.entity.Page;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
 
 @ApiModel(value="分页参数类",description="参数类" )
 @Data
@@ -20,4 +22,27 @@ public class PageDTO implements Serializable {
     @ApiModelProperty(value = "查询的开始行数",hidden  = true)
     protected  Integer fromLine;
 
+    public  int getFromLine(){
+        return (this.currentPage-1)*this.pageSize;
+    }
+
+
+
+    public <T> Page<T> toPage(List<T> list,Integer totalCount) {
+        Page<T> pageInfo = new Page<>();// 分页返回的总对象类型
+        cn.net.yzl.common.entity.PageParam pageParam = new cn.net.yzl.common.entity.PageParam();// 分页信息
+        pageParam.setPageNo(currentPage);// 当前页码
+        pageParam.setPageSize(pageSize);// 每页显示条目
+        pageParam.setTotalCount((int) totalCount);// 总记录数
+        int pageTotal = 0;
+        if (pageSize >= 0) {
+            pageTotal = (int) Math.ceil((double) (int) totalCount / (double) pageSize);// 计算总页数
+        }
+        pageParam.setPageTotal(pageTotal);
+        pageParam.setNextPage(pageTotal == currentPage ? 0 : currentPage + 1);// 下一页，没有下一页为0
+        pageParam.setPreviousPage(currentPage == 0 ? 0 : currentPage - 1);// 上一页，没有上一页为0
+        pageInfo.setItems(list);
+        pageInfo.setPageParam(pageParam);
+        return pageInfo;
+    }
 }
