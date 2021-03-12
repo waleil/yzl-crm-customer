e389bee681e3b859d9921f9ba22ea8e27a2cc0bb
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-03-12 13:53:16 +0800

    注释废弃方法

--- yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/dao/LabelMapper.java
+++ yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/dao/LabelMapper.java
@@ -1,26 +1,25 @@
-package cn.net.yzl.crm.customer.dao;
-
-import cn.net.yzl.crm.customer.model.Label;
-import cn.net.yzl.crm.customer.model.LabelGroup;
-import cn.net.yzl.crm.customer.model.LabelType;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Mapper
-public interface LabelMapper {
-
+//package cn.net.yzl.crm.customer.dao;
+//
+//import cn.net.yzl.crm.customer.model.Label;
+//import cn.net.yzl.crm.customer.model.LabelGroup;
+//import cn.net.yzl.crm.customer.model.LabelType;
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Select;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+//@Deprecated
+//@Mapper
+//public interface LabelMapper {
+//
 //     List<LabelGroup> getLabelGroups();
 //
-//     @Deprecated
 //     @Select("SELECT id, pid, `name`, field_name fieldName, `code`, value1, value2, label_type labelType,limit_dn limitDn, limit_up limitUp,\n" +
 //             "  sort, url, check_box checkBox, group_id groupId, route from  label")
 //     List<Label> getCustomerLabels();
 //
-//     @Deprecated
 //     @Select("SELECT id,`name` as labelName from  label_type_base")
 //     List<LabelType> selectAllLabelTypes();
-
-}
+//
+//}
