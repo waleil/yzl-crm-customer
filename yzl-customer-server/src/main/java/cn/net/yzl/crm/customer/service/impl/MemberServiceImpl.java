fec701b48493545d0fc17af912b2db641573aca0
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-03-18 10:28:56 +0800

    获取DMC会员到期时间配置增加try catch

--- yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/service/impl/MemberServiceImpl.java
+++ yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/service/impl/MemberServiceImpl.java
@@ -11,6 +11,7 @@ import cn.net.yzl.activity.model.responseModel.MemberLevelPagesResponse;
 import cn.net.yzl.common.entity.ComResponse;
 import cn.net.yzl.common.entity.Page;
 import cn.net.yzl.common.enums.ResponseCodeEnums;
+import cn.net.yzl.common.util.DateHelper;
 import cn.net.yzl.crm.customer.dao.*;
 import cn.net.yzl.crm.customer.dao.mongo.MemberCrowdGroupDao;
 import cn.net.yzl.crm.customer.dao.mongo.MemberLabelDao;
@@ -41,6 +42,7 @@ import cn.net.yzl.crm.customer.utils.CacheKeyUtil;
 import cn.net.yzl.crm.customer.utils.CentYuanConvertUtil;
 import cn.net.yzl.crm.customer.utils.MongoDateHelper;
 import cn.net.yzl.crm.customer.utils.RedisUtil;
+import cn.net.yzl.crm.customer.utils.date.DealDateUtil;
 import cn.net.yzl.crm.customer.viewmodel.MemberOrderStatViewModel;
 import cn.net.yzl.crm.customer.vo.*;
 import cn.net.yzl.crm.customer.vo.address.ReveiverAddressInsertVO;
@@ -1259,14 +1261,18 @@ public class MemberServiceImpl implements MemberService {
     //@Transactional
     public boolean updateMemberGrandValidityInit() throws IOException {//MemberSysParamDetailResponse
         //获取DMC的会员到期时间
-        String validDate = ActivityClientAPI.getMemberGradeValidDate();
-        if ("0".equals(validDate)) {
+        //String validDate = ActivityClientAPI.getMemberGradeValidDate();
+        MemberGradeValidDate validDateObj = ActivityClientAPI.getMemberGradeValidDateObj();
+        if (validDateObj.getIsAlways() == null || validDateObj.getIsAlways()) {
             log.info("updateMemberGrandValidityInit:会员有效期类型为长期有效！");
             return true;
         }
-        String today= DateUtil.today();
-        if (!today.equals(validDate)) {
-            log.info("updateMemberGrandValidityInit:会员未到期!当前时间为：{}，DMC会员到期时间为：{}！",today,validDate);
+        Date todayStart = DealDateUtil.getStart(new Date());
+
+        Date yearValidDate = validDateObj.getCurrentYearValidDate();
+        long between = DateUtil.between(todayStart,yearValidDate, DateUnit.DAY, false);
+        if (between != 0) {
+            log.info("updateMemberGrandValidityInit:会员未到期!当前时间为：{}，DMC会员到期时间为：{}！",todayStart,yearValidDate);
             return true;
         }
 
