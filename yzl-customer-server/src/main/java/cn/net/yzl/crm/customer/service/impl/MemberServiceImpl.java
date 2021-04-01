64db1ef1833bbd6bed80964ea8a1906a4d2c28b9
wangzhe <w-wangzhe@yuzhilin.net.cn>
2021-04-01 16:26:50 +0800

    订单签收的时候，让工单更新当前顾客的正在服用的商品的最小余量

--- yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/service/impl/MemberServiceImpl.java
+++ yzl-customer-server/src/main/java/cn/net/yzl/crm/customer/service/impl/MemberServiceImpl.java
@@ -881,6 +881,9 @@ public class MemberServiceImpl implements MemberService {
         List<MemberProductEffectInsertVO> addProductVoList = new ArrayList<>();
         List<MemberProductEffectUpdateVO> updateProductVoList = new ArrayList<>();
         List<MemberProductEffectDTO> productEffectList = null;
+        //当前顾客正在服用的所有商品的最小余量
+        Integer minProductLastNum = Integer.MAX_VALUE;
+
         if (CollectionUtil.isNotEmpty(buyProductList)) {
             for (OrderProductVO productVO : buyProductList) {
                 buyProductCodes.append(productVO.getProductCode()).append(",");
@@ -902,9 +905,6 @@ public class MemberServiceImpl implements MemberService {
                     productMap.put(mainDTO.getProductCode(), mainDTO);
                 }
             }
-
-            //当前顾客正在服用的所有商品的最小余量
-            Integer minProductLastNum = Integer.MAX_VALUE;
 
             //查询客户对应的商品服用效果
             MemberProductEffectSelectVO effectVo = new MemberProductEffectSelectVO();
@@ -1157,7 +1157,9 @@ public class MemberServiceImpl implements MemberService {
         member = memberMapper.selectMemberByCard(memberCard);
 
         //订单签收的时候，让工单更新当前顾客的正在服用的商品的最小余量
-        res = WorkOrderClientAPI.updateMinProductLastNum(memberCard, 1, String.valueOf(member.getMGradeId()));
+        if (!minProductLastNum.equals(Integer.MAX_VALUE)) {
+            res = WorkOrderClientAPI.updateMinProductLastNum(memberCard, minProductLastNum, String.valueOf(member.getMGradeId()));
+        }
 
         //设置缓存
         redisUtil.sSet(CacheKeyUtil.syncMemberLabelCacheKey(),memberCard);
